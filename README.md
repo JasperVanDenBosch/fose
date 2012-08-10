5acdc331f536e67b78f9c0fb6f7c8b5520d1a3e1
Jasper van den Bosch <jasper@Excalibur.(none)>
2012-08-10 13:14:13 +0200

    Added a console option to run compliance tests on a url.

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ fose
 ====
 
 Framework for Open Science Evaluation
+
+Background information: 
+http://ilogue.com/jasper/blog/fose--a-framework-for-open-science-evaluation/
+
+This package contains the technical documents for the FOSE framework
+and a library for testing and compliance purposes.
+
+To run the self-tests for this library:
+
+    fose --test
+
+To test a fose implementation at 'www.mydomain.com' for compliance:
+
+    fose --compliance http://www.mydomain.org
+
+
+
--- fose/compliance.py
+++ fose/compliance.py
@@ -1,21 +1,21 @@
 import unittest, sys
 
+
 def runtest(testclass):
     suite = unittest.TestLoader().loadTestsFromTestCase(testclass)
     testResult = unittest.TextTestRunner(verbosity=2).run(suite)
     if not testResult.wasSuccessful():
         sys.exit('[fose test wrapper] One or more tests failed!')
 
-def runall():
-    runtest(FoseTests)
 
-class FoseTests(unittest.TestCase):
+class ComplianceTests(unittest.TestCase):
 
-    def test_version(self):
-        import fose
-        import pkg_resources
-        pkgversion = pkg_resources.get_distribution("fose").version
-        self.assertEqual(fose.version,pkgversion)
+    def test_sth(self):
+        self.fail('Running compliance tests on url: {0} '.format(self.rootUrl))
+
+def run(targetUrl):
+    ComplianceTests.rootUrl = targetUrl
+    runtest(ComplianceTests)
 
 if __name__ == '__main__':
     unittest.main()
