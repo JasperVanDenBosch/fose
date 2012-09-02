e76f8737a16f13504fe65a12a95d0a982acf5286
jasper@ilogue <jasper@ilogue.com>
2012-09-02 23:15:19 +0200

    Added simple test data to compliance.

--- fose/compliance.py
+++ fose/compliance.py
@@ -4,6 +4,10 @@ import pkg_resources
 from lxml import etree
 import requests
 
+### Test Data Section
+
+TESTDOI = 'fosetest.1'
+
 
 def runtest(testclass):
     suite = unittest.TestLoader().loadTestsFromTestCase(testclass)
@@ -11,7 +15,7 @@ def runtest(testclass):
     if not testResult.wasSuccessful():
         sys.exit('[fose test wrapper] One or more tests failed!')
 
-
+# These tests assume that the compliance test data have been installed
 class ComplianceTests(unittest.TestCase):
 
     def setUp(self):
@@ -19,7 +23,7 @@ class ComplianceTests(unittest.TestCase):
 
     def test_Get_review_thread_for_id(self):
 		#use protocol url to GET response 
-        url = self.uri.forDoi('fosetest.1')
+        url = self.uri.forDoi(TESTDOI)
         #actually get file
         response = requests.get(url, headers={'Accept':'application/xml'})
         response.raise_for_status()
@@ -31,7 +35,6 @@ class ComplianceTests(unittest.TestCase):
         root = etree.fromstring(response.text, parser)
 		#use core lib to read this as a model object
 		#test for properties
-        self.fail('Running compliance tests on url: {0} '.format(url))
 
 def run(targetUrl):
     ComplianceTests.rootUrl = targetUrl
